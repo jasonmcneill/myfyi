c1208197a06765b5f06f938cb68c687bfcff8e96
Jason McNeill <vrtjason@gmail.com>
2026-04-27 17:54:34 -0700

    Fix bug

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-require("dotenv").config();
+require("dotenv").config({ override: true });
 
 const errorHandler = require("./middleware/errorHandler");
 
